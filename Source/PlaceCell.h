ca7e76c438166fade5d2de87d73743e2cf4b8f62
Mark Barrasso <mark.barrasso@live.com>
2014-08-31 23:26:37 -0400

    main scene, tableview, placecell created

---
+++ Source/PlaceCell.h
@@ -0,0 +1,13 @@
+//
+//  PlaceCell.h
+//  WhatsBusy
+//
+//  Created by Mark on 8/31/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface PlaceCell : CCNode
+
+@end
---
+++ Source/PlaceCell.m
@@ -0,0 +1,13 @@
+//
+//  PlaceCell.m
+//  WhatsBusy
+//
+//  Created by Mark on 8/31/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "PlaceCell.h"
+
+@implementation PlaceCell
+
+@end
